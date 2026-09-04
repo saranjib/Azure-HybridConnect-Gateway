812fe21ae31ee038ab3ee3ebfdcdf52301e4bc26
Saranjib Kuanar <102132535+saranjib@users.noreply.github.com>
2026-09-04 16:53:57 +0530

    Add variables for resource group and location

--- variables.tf
+++ variables.tf
@@ -1,1 +1,11 @@
+variable "resource_group_name" {
+  description = "Azure Resource Group name"
+  type        = string
+  default     = "hybrid-connect-gateway-project"
+ }
 
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "Central India"
+}
