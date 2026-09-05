e88861b80bfde0dfcc5148adf8d650292ced2f01
Saranjib Kuanar <102132535+saranjib@users.noreply.github.com>
2026-09-05 11:41:31 +0530

    Add Azure virtual network and subnets resources

--- network.tf
+++ network.tf
@@ -1,1 +1,30 @@
+resource "azurerm_virtual_network" "hybrid-connect-gateway-vnet" {
+  name                = "hybrid-connect-gateway-vnet"
+  location            = azurerm_resource_group.hybrid-connect-gateway-project.location
+  resource_group_name = azurerm_resource_group.hybrid-connect-gateway-project.name
+
+  address_space = var.vnet_address_space
+
+}
+
+resource "azurerm_subnet" "gateway" {
+  name                 = "GatewaySubnet"
+  resource_group_name  = azurerm_resource_group.hybrid-connect-gateway-project.name
+  virtual_network_name = azurerm_virtual_network.hybrid-connect-gateway-vnet.name
+
+  address_prefixes = [
+    var.gateway_subnet_prefix
+  ]
+}
+
+resource "azurerm_subnet" "private" {
+  name                 = "private-subnet"
+  resource_group_name  = azurerm_resource_group.hybrid-connect-gateway-project.name
+  virtual_network_name = azurerm_virtual_network.hybrid-connect-gateway-vnet.name
+
+  address_prefixes = [
+    var.private_subnet_prefix
+  ]
+}
+
 
